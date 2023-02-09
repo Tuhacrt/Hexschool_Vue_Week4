aabefebad57fd75df4cf0c722bd8387b4b466022
Tuhacrt <a067800497@gmail.com>
2023-02-09 12:22:38 +0800

    Change base url

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: "Hexschool_Vue_Week4",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
